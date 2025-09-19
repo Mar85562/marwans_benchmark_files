9a50cee3de07a906687db120ea17070e9e548565
Mar85562 <135610199+Mar85562@users.noreply.github.com>
2025-09-18 20:56:05 -0400

    fixed git tracking issues; cached data was showing on github respository

--- subtractors/unsigned/32b/sub_32bit_unsigned.v
+++
@@ -1,9 +0,0 @@
-module sub_32bit_unsigned (
-    input  wire [31:0] A,       // first input (subtract from)
-    input  wire [31:0] B,       // number to subtract
-    output wire [31:0] result,  // A - B (mod 2^32)
-    output wire        borrow   // 1 when A < B
-);
-    assign result = A - B;
-    assign borrow = (A < B);
-endmodule
--- subtractors/unsigned/8b/sub_8bit_unsigned.v
+++
@@ -1,9 +0,0 @@
-module sub_8bit_unsigned (
-    input  wire [7:0] A,       // first input (subtract from)
-    input  wire [7:0] B,       // number to subtract
-    output wire [7:0] result,  // A - B (mod 2^8)
-    output wire       borrow   // 1 when A < B
-);
-    assign result = A - B;
-    assign borrow = (A < B);
-endmodule
